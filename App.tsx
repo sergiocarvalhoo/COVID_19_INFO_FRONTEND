71bf6a0e5c0b079d214493136f155a706964e15b
Sérgio Carvalho <sergiotutorshd@gmail.com>
2021-09-09 23:52:57 -0300

    Add Welcome Screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Button, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { AppRegistry, StatusBar, StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'white',
+    accent: '#FF5B5B',
+    background: 'blue',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World !</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <PaperProvider theme={theme}>
+
+      <View style={styles.container}>
+
+        <Icon
+          style={styles.icon}
+          name="local-hospital"
+          size={128}
+          color="#FFF"
+        />
+
+        <Text style={styles.title}>COVID 19 - INFO</Text>
+        <Text style={styles.welcome}>Aplicativo de Notícias e Informações do Covid-19, na Cidade de Cajazeiras - PB.</Text>
+
+
+        <Button icon="play" mode="contained" onPress={() => console.log('Pressed')}>
+        Iniciar
+        </Button>
+
+      </View>
+
+   </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#80ff80',
+    backgroundColor: '#FF5B5B',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  icon: {
+    paddingBottom: 30
+  },
+  title: {
+    fontSize: 36,
+    fontFamily: 'bold',
+    color: 'white',
+    paddingBottom: 40,
+    textAlign: 'center'
+  },
+  welcome: {
+    fontSize: 18,
+    fontFamily: 'bold',
+    color: 'white',
+    paddingBottom: 20,
+    textAlign: 'center'
+  }
 });
