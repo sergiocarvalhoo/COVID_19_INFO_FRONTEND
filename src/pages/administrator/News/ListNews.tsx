32df99930d2afc0cc156d984046b05d79b6cdca7
fernando <warfernand@gmail.com>
2021-10-02 01:10:46 -0300

    List View

--- src/pages/administrator/News/ListNews.tsx
+++ src/pages/administrator/News/ListNews.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, DefaultTheme, Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {useNavigation} from '@react-navigation/native';
-import HeaderRedLogged from '../../../components/Headers/HeaderRedLogged'; 
+import { useFocusEffect } from '@react-navigation/native';
+import apiConnection from '../../../services/ApiConnection';
+import HeaderRedLogged from '../../../components/Headers/HeaderRedLogged';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 
 const theme = {
@@ -11,36 +13,68 @@ const theme = {
   colors: {
     ...DefaultTheme.colors,
     primary: 'white',
-    accent: '#FF5B5B',
-    background: 'blue',
+    accent: 'white'
   },
 };
 
-export default function News() {
 
-  const navigation = useNavigation();
+interface News {
+  id: number;
+  title: string;
+  description: string;
+  publication_date: Date;
+}
+
+
+export default function ListNews() {
+
+  const [news, setNews] = useState<News[]>([]);
+
+  useFocusEffect(() => {
+    apiConnection.get('/readnews').then(response => {
+      setNews(response.data);
+    })
+  })
 
   return (
 
     <PaperProvider theme={theme}>
 
-      <HeaderRedLogged titulo="Fazer Logout"/> 
+      <HeaderRedLogged titulo="Fazer Logout" />
 
       <View style={styles.container}>
 
-        <Text style={styles.title}>Noticías Administrator</Text>
+        <Text style={styles.title}>Lista de Notícias</Text>
 
-        <Icon
-          style={styles.icon}
-          name="local-hospital"
-          size={128}
-          color="#000"
-        />
+        <ScrollView>
+
+          {
+            news.map(news =>
+
+              <View style={styles.item} key={news.id}>
+
+                <Text style={styles.textItem}>{news.title}</Text>
+                <Text style={styles.textItem}>Data da Publicação: {news.publication_date}</Text>
+
+                <BorderlessButton>
+                  <Icon name="delete-forever" size={32} color="black" />
+                </BorderlessButton>
+
+                <BorderlessButton>
+                  <Icon name="update" size={32} color="black" />
+                </BorderlessButton>
+
+              </View>
+
+            )
+          }
+
+        </ScrollView>
 
       </View>
 
-   </PaperProvider>
-   
+    </PaperProvider>
+
   );
 }
 
@@ -48,17 +82,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
     justifyContent: 'center',
   },
   icon: {
     paddingBottom: 30
   },
   title: {
-    fontSize: 36,
+    fontSize: 25,
     fontFamily: 'Roboto',
     color: '#000',
-    paddingBottom: 40,
+    paddingTop: 15,
+    paddingBottom: 20,
     textAlign: 'center'
+  },
+  item: {
+    backgroundColor: '#BDBDBD',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 5,
+    margin: 2,
+    borderColor: '#2a4944',
+    borderWidth: 2
+  },
+  textItem: {
+    fontSize: 18,
+    fontFamily: 'Roboto',
+    //fontWeight: 'bold',
   }
 });
