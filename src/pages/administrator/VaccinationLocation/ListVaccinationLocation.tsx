3f0dd894679b1608fcd72e95a7065f0be05425ef
glaymar2010@live.com <glaymar2010@live.com>
2021-10-04 17:44:01 -0300

    feat: exibir mapa

--- src/pages/administrator/VaccinationLocation/ListVaccinationLocation.tsx
+++ src/pages/administrator/VaccinationLocation/ListVaccinationLocation.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import {useNavigation} from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, DefaultTheme, FAB, Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
+import apiConnection from '../../../services/ApiConnection';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+
+import MapView, { MapEvent, Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 import HeaderRedLogged from '../../../components/Headers/HeaderRedLogged';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+//import {maker} from '../../../../assets/map-maker@2x.png' ;
 
 
 const theme = {
@@ -11,54 +17,245 @@ const theme = {
   colors: {
     ...DefaultTheme.colors,
     primary: 'white',
-    accent: '#FF5B5B',
-    background: 'blue',
+    accent: 'white'
   },
 };
 
-export default function VaccinationLocal() {
+interface vaccinationLocations {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  description: string;
+}
+
+const LocationMap: React.FC = () => {
 
   const navigation = useNavigation();
 
+  const [vaccinationLocations, setLocations] = useState<vaccinationLocations[]>([])
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [name, setName] = useState('');
+
+
+  useFocusEffect(() => {
+    apiConnection.get('vaccinationlocations').then(response => {
+      setLocations(response.data);
+    });
+  });
+
+  function handleSelectMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
+  }
+
+  function handleAddLocation(latitude: number, longitude: number, name: string) {
+    const data = {
+      name,
+      description: 'Ponto de Vacinação',
+      latitude,
+      longitude
+
+    }
+    apiConnection.post('createvaccinationlocation', { data });
+    console.log(data);
+  }
+
+  function handleDeleteLocation(id: number) {
+    apiConnection.delete('/deletevaccinationlocation', { data: { id: `${id}` } });
+    console.log(id);
+  }
+
+  function handleUpdateNews(latitude: number, longitude: number, name: string) {
+    const data = {
+      name,
+      description: 'Ponto de Vacinação',
+      latitude,
+      longitude
+    }
+
+    apiConnection.put('/updatevaccinationlocation', data)
+  };
+
+
   return (
-   
     <PaperProvider theme={theme}>
 
-      <HeaderRedLogged titulo="Fazer Logout"/>
+      <HeaderRedLogged titulo="Fazer Logout" />
 
       <View style={styles.container}>
 
-        <Text style={styles.title}>Locais de Vacinação Administrator</Text>
+        <MapView
+          initialRegion={{
+            latitude: -6.892061028520502,
+            longitude: -38.558394175307164,
+            latitudeDelta: 0.008,
+            longitudeDelta: 0.008,
+          }}
+          style={styles.mapStyle}
+          onPress={handleSelectMapPosition}
+        >
+          {
+            vaccinationLocations.map(vaccinationLocations =>
+              <Marker key={vaccinationLocations.id} coordinate={{
+                latitude: vaccinationLocations.latitude,
+                longitude: vaccinationLocations.longitude,
+              }}
+              >
+                <Callout>
+                  <Card>
+                    <Text>
+                      {vaccinationLocations.name}
+                      ({vaccinationLocations.description})
+                    </Text>
+                  </Card>
+                  <BorderlessButton onPress={() => { handleDeleteLocation(vaccinationLocations.id) }}>
+                    <Icon name="delete-forever" size={32} color="black" />
 
-        <Icon
-          style={styles.icon}
-          name="local-hospital"
-          size={128}
-          color="#000"
-        />
+                  </BorderlessButton>
+
+                </Callout>
+
+
+              </Marker>
+
+            )
+          }
+
+          <Marker
+            pinColor="black"
+            coordinate={{
+              latitude: position.latitude,
+              longitude: position.longitude,
+            }}
+
+          />
+
+        </MapView>
+
+
+        <View style={styles.container1}>
+
+          <TextInput
+            style={styles.footerText}
+            label="Digite o Nome do Local de Vacinação:"
+            value={name}
+            selectionColor='#FF5B5B'
+            underlineColor='#FF5B5B'
+            outlineColor='#FF5B5B'
+            onChangeText={setName}
+          />
+
+          <Button
+            style={styles.button}
+            icon="map"
+            color='#FF5B5B'
+            mode="contained"
+            onPress={() => { handleAddLocation(position.latitude, position.longitude, name) }}>
+            Adicionar
+          </Button>
+
+        </View>
 
       </View>
-
-   </PaperProvider>
-
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    position: 'relative'
+  },
+
+
+  container1: {
+    margin: 'auto' ,
+    display: 'flex',
     alignItems: 'center',
+    justifyContent: 'space-between',
+
+    //footer
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 3,
+    
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    elevation: 3,
+   
+    // // borderRadius: 28,
+    // height: 50,
+    // paddingLeft: 50,
+   
+
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#15c3d6",
+    borderRadius: 28,
+
     justifyContent: 'center',
+    alignItems: 'center'
   },
-  icon: {
-    paddingBottom: 30
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#fff",
+    // borderRadius: 28,
+    height: 46,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+
+    elevation: 3,
   },
-  title: {
-    fontSize: 36,
-    fontFamily: 'Roboto',
-    color: '#000',
-    paddingBottom: 40,
-    textAlign: 'center'
+  footerText: {
+    color: '#8fa7b3',
+    fontFamily: 'Nunito_700Bold',
+    alignItems: 'center',
+    height: 25,
+    width: 200,
+
+    justifyContent: 'space-between',
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#FFF',
+  },
+  button: {
+    marginTop: 0,
+    color: '#FF5B5B',
+    alignItems: 'flex-start'
   }
 });
+
+export default LocationMap;
--- src/pages/public/VaccinationLocation/ListVaccinationLocation.tsx
+++ src/pages/public/VaccinationLocation/ListVaccinationLocation.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import {useNavigation} from '@react-navigation/native';
-import HeaderRed from '../../../components/Headers/HeaderRed';
+import React, { useEffect, useState } from 'react';
+import { Button, DefaultTheme, FAB, Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
+import apiConnection from '../../../services/ApiConnection';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+
+import MapView, { MapEvent, Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import HeaderRedLogged from '../../../components/Headers/HeaderRedLogged';
+//import {maker} from '../../../../assets/map-maker@2x.png' ;
 
 
 const theme = {
@@ -11,54 +16,160 @@ const theme = {
   colors: {
     ...DefaultTheme.colors,
     primary: 'white',
-    accent: '#FF5B5B',
-    background: 'blue',
+    accent: 'white'
   },
 };
 
-export default function VaccinationLocal() {
+interface vaccinationLocations {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+  description: string;
+}
 
+const LocationMap: React.FC = () => {
   const navigation = useNavigation();
 
-  return (
+  const [vaccinationLocations, setLocations] = useState<vaccinationLocations[]>([])
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [name, setName] = useState('');
 
+  // sempre que o usuário sair e voltar da tela, ela é disparada
+  useFocusEffect(() => {
+    apiConnection.get('vaccinationLocations').then(response => {
+      setLocations(response.data);
+    });
+  });
+
+
+  function handleSelectMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
+  }
+
+  function handleAddLocation(latitude:number , longitude:number ,name: string) {
+    const data = {
+      name,
+      description: 'Ponto de Vacinação',
+      latitude,
+      longitude
+     
+    }
+    apiConnection.post('vaccinationLocations',{data})
+  }
+
+  return (
     <PaperProvider theme={theme}>
 
-      <HeaderRed titulo="Voltar para Tela Inicial" navigationPage="Welcome"/>
-
+      <HeaderRedLogged titulo="Fazer Logout" />
       <View style={styles.container}>
+        <MapView
+          initialRegion={{
+            latitude: -6.892061028520502,
+            longitude: -38.558394175307164,
+            latitudeDelta: 0.008,
+            longitudeDelta: 0.008,
+          }}
+          style={styles.mapStyle}
+          onPress={handleSelectMapPosition}
+        >
+          {
+            vaccinationLocations.map(vaccinationLocations =>
+              <Marker key={vaccinationLocations.id} coordinate={{
+                latitude: vaccinationLocations.latitude,
+                longitude: vaccinationLocations.longitude,
+              }}
+              >
+                <Callout>
+                  <Text>
+                    {vaccinationLocations.name}
+                  </Text>
+                </Callout>
+              </Marker>)
+          }
+          {/* <Marker
+            coordinate={{
+              latitude: position.latitude,
+              longitude: position.longitude,
+            }}
+          ></Marker> */}
 
-        <Text style={styles.title}>Locais de Vacinação</Text>
 
-        <Icon
-          style={styles.icon}
-          name="local-hospital"
-          size={128}
-          color="#000"
-        />
+        </MapView>
 
+
+        {/* <View style={styles.footer}>
+          <TextInput value={name} onChangeText={text => setName(text)} />  Add Local 
+
+          <RectButton style={styles.createOrphanageButton} onPress={() => {handleAddLocation(position.latitude, position.longitude, name )}} >
+            <Feather name="plus" size={20} color="#fff" />
+          </RectButton>
+
+        </View> */}
       </View>
-
-   </PaperProvider>
-
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
+    position: 'relative'
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#15c3d6",
+    borderRadius: 28,
+
     justifyContent: 'center',
+    alignItems: 'center'
   },
-  icon: {
-    paddingBottom: 30
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#fff",
+    borderRadius: 28,
+    height: 46,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
   },
-  title: {
-    fontSize: 36,
-    fontFamily: 'Roboto',
-    color: '#000',
-    paddingBottom: 40,
-    textAlign: 'center'
+  footerText: {
+    color: '#8fa7b3',
+    fontFamily: 'Nunito_700Bold'
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#FFF',
   }
 });
+
+export default LocationMap;
